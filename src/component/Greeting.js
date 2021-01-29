35cbadc4f238701c9f1aed4a3b8899dfc0f1303a
BoolAid-Man <marcouxaustin@gmail.com>
2021-01-29 18:12:27 -0500

    Making sure everything is up to date

--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Greeting(name: String) {
+function Greeting(name) {
     console.log("Generating Greeting with the following name");
     name = name.name.trim();
     console.log(name);
