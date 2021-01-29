51d3629d6e5d04304a4dc7571a465a28fa3e21eb
BoolAid-Man <marcouxaustin@gmail.com>
2021-01-29 18:03:21 -0500

    Removed unused import that was preventing Netlify Build

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
